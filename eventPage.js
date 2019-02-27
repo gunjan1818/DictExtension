164c7dabeb1bc416980d88835e3713e130b7328e
gunjan1818 <43075281+gunjan1818@users.noreply.github.com>
2019-02-28 01:12:08 +0530

    DictExtension

---
+++ eventPage.js
@@ -0,0 +1,31 @@
+var menuItem = {
+	"id" : "wikit",
+	"title": "Wikit",
+	"contexts": ["selection"]
+};
+
+chrome.contextMenus.create(menuItem);
+
+function fixedEncodeURI(str) {
+	return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+
+	if (clickData.menuItemId == "wikit" && clickData.selectionText){
+		var wikiUrl = "https://dictionary.cambridge.org/dictionary/english/" + fixedEncodeURI(clickData.selectionText);
+		var createData = {
+			"url" : wikiUrl,
+			"type": "popup",
+			"top": 5,
+			"left": 5,
+			"width": screen.availWidth/2,
+			"height": screen.availWidth/2
+		};
+
+		chrome.windows.create(createData,function(){})
+	}		
+})
+
+//https://dictionary.cambridge.org/dictionary/english/
+//https://en.wikipedia.org/wiki/
